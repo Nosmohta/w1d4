7ba7f6b1ceb17b6ed69b272ef9d56e77724393d0
Andrew Thomson <Thomson.w.andrew@gmail.com>
2017-05-04 17:56:30 +0000

    Waldo Funciton refactored with forEach

---
+++ find-waldo.js
@@ -0,0 +1,29 @@
+/* Old version
+function findWaldo(arr, found) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i, arr[i]);
+    }
+  }
+}
+function actionWhenFound(index, value) {
+  console.log("Waldo was found at index:", index, ". Proof of life value ==>", value);
+}
+*/
+
+
+function findWaldo(arr) {
+  arr.forEach( (name, index) => {
+     if (name === "Waldo") {
+      console.log("Waldo was found at index:", index, ". Proof of life value ==>", name)
+    }
+  })
+}
+
+
+let people = ["Alice", "Bob", "Waldo", "Winston", "sdkjfg", "Waldo"]
+
+findWaldo(people);
+
+
+
